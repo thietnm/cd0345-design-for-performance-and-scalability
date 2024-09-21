ee2bd748f886f927e48f2fb0b4932f6b271e3e10
MacOS <macos@Nguyen-Minh-Thiet.local>
2024-09-21 16:51:09 +0700

    add files

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,25 @@
 # TODO: Designate a cloud provider, region, and credentials
-
+provider "aws" {
+  profile = "default"
+  region = "us-east-1"
+}
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
-
+resource "aws_instance" "Udacity_T2" {
+  count = "4"
+  ami = "ami-0ed6534c7d6a8e78f"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Udacity T2"
+  }
+}
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
+#  resource "aws_instance" "Udacity_M4" {
+#    count = "2"
+#    ami = "ami-0ed6534c7d6a8e78f"
+#    instance_type = "m4.large"
+#    tags = {
+#     Name = "Udacity M4"
+#   }
+# }
--- Exercise_2/variables.tf
+++ Exercise_2/variables.tf
@@ -1,1 +1,7 @@
 # TODO: Define the variable for aws_region
+variable "lambda_name" {
+  default = "greet_lambda"
+}
+variable "lambda_output_path" {
+  default = "output.zip"
+}
